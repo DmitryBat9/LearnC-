e81a25a0e07664276e3d7a6b51ee4b458ad75f61
DmitryBat9 <snaiper2000asa@gmail.com>
2025-11-17 10:52:07 +0500

    Add files via upload

---
+++ Задачи по C++. Модуль 1.4.cpp
@@ -0,0 +1,269 @@
+﻿//-----Решенные задачи по модулю "1.4 Условный оператор" -----//
+
+#include <iostream>
+
+using namespace std;
+
+
+// Наибольшее из двух - 1
+/*
+int main()
+{
+	int a,b;
+	cin >> a >>b;
+	if (a > b) {
+		cout << a;
+	}
+	if (a < b) {
+		cout << b;
+	}
+	if (a == b) {
+		cout << a;
+	}
+}
+*/
+
+// Наибольшее из двух - 1
+/*
+int main()
+{
+	int a,b;
+	cin >> a >> b;
+	if (a > b) {
+		cout << 1;
+	}
+	if (a < b) {
+		cout << 2;
+	}
+	if (a == b) {
+		cout << 0;
+	}
+}
+*/
+
+// Наибольшее из трех
+/*
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a >= b and a >= c) {
+		cout << a;
+	}
+	else if  (b >= a and b >= c) {
+		cout << b;
+	}
+	else if (c >= a and c >= b) {
+		cout << c;
+	}
+	else {
+		cout << a;
+	}
+}
+*/
+
+// Треугольник A,B,C
+/*
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a < c and b < c and a + b > c) {
+		cout << "YES";
+	}
+	else if  (a < b and c < b and a + c > b) {
+		cout << "YES";
+	}
+	else if (c < a and b < a and b + c > a) {
+		cout << "YES";
+	}
+	else if (a == b and a == c and b == c) {
+		cout << "YES";
+	}
+	else {
+		cout << "NO";
+	}
+}
+*/
+
+// Совпадение среди трех чисел
+/*
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a == c and b == c and a == c) {
+		cout << "3";
+	}
+	else if  ((a == b or a == c ) or (b == a or b == c) or (c == a or c == b)) {
+		cout << "2";
+	}
+	else {
+		cout << "0";
+	}
+}
+*/
+
+// Ход шахматной ладьи
+/*
+int main()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if ((a == c and b != d) or (b == d and a != c)) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    } 
+}
+*/
+
+// Ход шахматного короля
+/*
+int main()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if ((c == a - 1 and b == d) or (c == a - 1 and d == b + 1) or (c == a and d == b + 1) or (c == a + 1 and d == b + 1) or (c == a + 1 and d == b) or (c == a + 1 and d == b - 1) or (c == a and d == b - 1) or (c == a - 1 and d == b - 1) ) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+}
+*/
+
+// Ход шахматного слона
+/*
+int main()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (abs(c - a) == abs(d - b)) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+}
+*/
+
+// Ход шахматного ферзя
+/*
+int main()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if ((abs(c - a) == abs(d - b)) or (a == c ) or (b == d)) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+}
+*/
+
+// Ход шахматного коня
+/*
+int main()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if ((abs(a - c) == 2 and abs(b - d) == 1) or (abs(a - c) == 1 and abs(b - d) == 2)) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+}
+*/
+
+// Шоколадка MxN
+/*
+int main()
+{
+    int n, m, k;
+    cin >> n >> m >> k;
+    if ((n == k or k % n == 0 and k <= n * m - n) or (m == k or k % m == 0 and k <= n * m - m)) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+}
+*/
+
+// Яша и бассейн MxN
+/*
+int main() {
+  int n, m, x, y;
+    cin >> n >> m >> x >> y;
+    if (n > m) {
+        n = n + m;
+        m = n - m;
+        n = n - m;
+    }
+    if (2 * x > n) {
+        x = n - x;
+    }
+    if (2 * y > m) {
+        y = m - y;
+    }
+    if (x < y) {
+        cout << x;
+    } else {
+        cout << y;
+    }
+  return 0;
+}
+*/
+
+// Упорядочить три числа по неубыванию
+/*
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    
+    if (a <= b && a <= c) {
+        if (b <= c) {
+            cout << a << " " << b << " " << c;
+        } else {
+            cout << a << " " << c << " " << b;
+        }
+    } else if (b <= a && b <= c) {
+        if (a <= c) {
+            cout << b << " " << a << " " << c;
+        } else {
+            cout << b << " " << c << " " << a;
+        }
+    } else {
+        if (a <= b) {
+            cout << c << " " << a << " " << b;
+        } else {
+            cout << c << " " << b << " " << a;
+        }
+    }
+    
+    return 0;
+}
+*/
+
+// Коробки 
+/*
+int main() {
+    int a, b, c, x, y, z, t;
+    cin >> a >> b >> c >> x >> y >> z;
+    if (a > b) t = a, a = b, b = t;
+    if (b > c) t = b, b = c, c = t;
+    if (a > b) t = a, a = b, b = t;
+    if (x > y) t = x, x = y, y = t;
+    if (y > z) t = y, y = z, z = t;
+    if (x > y) t = x, x = y, y = t;
+    if (a == x && b == y && z == c) cout << "Boxes are equal";
+    else if (a <= x && b <= y && c <= z) cout << "The first box is smaller than the second one";
+    else if (a >= x && b >= y && c >= z) cout << "The first box is larger than the second one";
+    else cout << "Boxes are incomparable";
+    return 0;
+}
+*/
---
+++ Задачи по C++. Модуль 1.5.cpp
@@ -0,0 +1,244 @@
+﻿//-----Решенные задачи по модулю "1.5 Цикл while" -----//
+
+#include <iostream>
+
+using namespace std;
+
+
+// Квадраты натуральных чисел
+/*
+int main()
+{
+    int i,N;
+    cin >> N;
+    i = 1;
+    while (i * i <= N) {
+        cout << i * i << " ";
+        i = i + 1;
+    }
+}
+*/
+
+// Наименьший натуральный делитель
+/*
+int main()
+{
+    int i,N;
+    cin >> N;
+    i = 2;
+    while (N % i != 0) {
+        i = i + 1;
+    }
+    cout << i;
+}
+*/
+
+// Степени двойки
+/*
+int main()
+{
+    int i, N;
+    cin >> N;
+    i = 1;
+    while (i <= N) {
+        cout << i << " ";
+        i = i*2;
+    }
+}
+*/
+
+// Проверка точной степени двойки
+/*
+int main()
+{
+    int N;
+    cin >> N;
+    while (N % 2 == 0) {
+        N = N / 2;
+    }
+    if (N == 1) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+}
+*/
+
+// Количество элементов последовательности
+/*
+int main()
+{
+    int N, i;
+    cin >> N;
+    i = 0;
+    while (N != 0) {
+        cin >> N;
+        i = i + 1;
+    }
+    cout << i;
+}
+*/
+
+// Сумма элементов последовательности
+/*
+int main()
+{
+    int N, sum;
+    cin >> N;
+    sum = N;
+    while (N != 0) {
+        cin >> N;
+        sum = sum + N;
+    }
+    cout << sum;
+}
+*/
+
+// Наибольший элемент последовательности
+/*
+int main()
+{
+    int N;
+    cin >> N;
+    int max = N;
+    while (N != 0) {
+        cin >> N;
+        if (N > max) {
+            max = N;
+        }
+    }
+    cout << max;
+}
+*/
+
+// Количество наибольших элементов последовательности
+/*
+int main()
+{
+    int N,i;
+    cin >> N;
+    i = 1;
+    int max = N;
+    while (N != 0) {
+        cin >> N;
+        if (N > max) {
+            max = N;
+            i = 1;
+        }
+        else if (N == max) {
+            i = i + 1;
+            }
+    }
+    cout << i;
+}
+*/
+
+// Второй по величине элемент последовательности
+/*
+int main()
+{
+    int N;
+    int max1 = 0;
+    int pr_max = 0;
+    while (cin >> N && N != 0) {
+        if (N >= max1) {
+            pr_max = max1;
+            max1 = N;
+        }
+        else if (N > pr_max) {
+            pr_max = N;
+        }
+    }
+    std::cout << pr_max;
+}
+*/
+
+// Последовательность Фибоначчи - 1
+/*
+int main()
+{
+	int n, ff0, i = 1, f = 1, f0 = 0;
+    cin >> n;
+    while (i < n) {
+        ff0 = f;
+        f = f0 + f;
+        f0 = ff0;
+        i = i + 1;
+}
+    cout << f;
+}
+*/
+
+// Последовательность Фибоначчи - 2
+/*
+int main() { 
+    int F0=0,F1=1,A,Fn=0,n=1;
+    cin >> A;
+    while(Fn < A) { 
+        Fn = F0 + F1;
+        F0 = F1;
+        F1 = Fn;
+        n++;
+    }
+    if (A == Fn) cout << n;
+    else cout << -1;
+    return 0;
+}
+*/
+
+// Наибольшее число подряд идущих элементов последовательности
+/*
+int main() {
+
+	int n, m, i = 0, j = 0;
+	cin >> n;
+
+	while (n != 0) {
+		m = n;
+		while (m == n) cin >> n, i++;
+		if (i > j) j = i;
+		i = 0;
+	}
+
+	cout << j;
+	
+	return 0;
+}
+*/
+
+// Локальный максимум последовательности
+/*
+int main() {
+	int a = 0, b = 0, c = 0, n;
+
+	while (cin >> n && n != 0) {
+		if (a > 0 && a < b && b > n)
+			c++;
+		a = b; b = n;
+	}
+
+	cout << c;
+	return 0;
+}
+*/
+
+// Расстояние между двумя локальными максимумами
+/*
+int main() {
+	int a, b, c, n = 0, x = 0;
+	cin >> a >> b >> c;
+	while (c != 0) {
+		if (a < b && b > c) {
+			if (x == 0) x = n, n = 1;
+			else if (x >= n) x = n, n = 1;
+			else n = 1;
+		}
+		else if (n > 0)  n = n + 1; 
+		a = b, b = c;
+		cin >> c;
+	}
+	cout << x;
+	return 0;
+}
+*/
